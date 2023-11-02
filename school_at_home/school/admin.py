d45fe0e71373bad39a35191f9fed2c0213f0bef5
tobyhui22 <tobyhui22@gmail.com>
2023-11-02 10:10:05 +0000

    complete the admin

--- school_at_home/school/admin.py
+++ school_at_home/school/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import school, student, ClassName, teacher
 
-# Register your models here.
+admin.site.register(school)
+admin.site.register(student)
+admin.site.register(ClassName)
+admin.site.register(teacher)
+
--- school_at_home/school/models.py
+++ school_at_home/school/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class school(models.Model):
+    School_Name = models.CharField(max_length=50, help_text='School Name')
+    School_Address = models.CharField(max_length=100, help_text='School Address')
+    School_Telephone = models.CharField(max_length=20, help_text='Telephone')
+    School_Email = models.EmailField()
+
+    def __str__(self):
+        return self.School_Name
+
+class ClassName(models.Model):
+    ClassName = models.CharField(max_length=20, help_text='Class Name')
+    schoolID = models.ForeignKey('school', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.ClassName
+    
+class student(models.Model):
+    Student_First_Name = models.CharField(max_length=20, help_text='First Name')
+    Student_Last_Name = models.CharField(max_length=20, help_text='Last Name')
+    student_dateOfBirth = models.DateField()
+    Gen = (
+        ('M', 'Male'),
+        ('F', 'Female')
+    )
+    student_gender = models.CharField(max_length=10, choices=Gen)
+    schoolID = models.ForeignKey('school', on_delete=models.CASCADE, null=True)
+    classID = models.ForeignKey('ClassName', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.Student_First_Name} {self.Student_Last_Name}"
+
+class teacher(models.Model):
+    teacher_firstName = models.CharField(max_length=20, help_text='Teacher First Name')
+    teacher_lastName = models.CharField(max_length=20, help_text='Teacher Last Name')
+    teacher_email = models.EmailField()
+    Ext = (
+        ('mr', 'Mr.'),
+        ('ms', 'Ms.'),
+        ('miss', 'Miss')
+    )
+    extension = models.CharField(max_length=10, choices=Ext)
+    classID = models.ManyToManyField('ClassName')
+
+    def __str__(self):
+        return f"{self.extension} {self.teacher_lastName}"
+
